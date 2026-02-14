b5b563c401db6e18b1ac1c558aaa7fb5b458c490
abdulazizovv <aqllibola567@gmail.com>
2026-02-14 15:44:48 +0500

    initial commit

---
+++ post/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Kategoriya"
+        verbose_name_plural = "Kategoriyalar"
+        ordering = ("title",)
+
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.CharField(max_length=255, blank=True, null=True)
+    body = models.TextField()
+    status_choices = (
+        ("draft", "Qoralama"),
+        ("public", "Ommaviy"),
+        ("deleted", "O'chirilgan"),
+        ("archived", "Arxivlangan")
+    )
+
+    status = models.CharField(max_length=15, choices=status_choices)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[:50] + "..." if len(self.body) > 50 else self.body
+    
+
